64ae8d4f731d0a2ca3e5e6093ecacb7d061b3522
vivo1806 <vivekkumarmore44@gmail.com>
2026-02-26 16:13:33 +0530

    initial project setup for bitespeed-identity

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
